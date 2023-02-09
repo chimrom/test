02e63a3601114964a40c8807eb5a8d7ac6aef845
chimrom <116731372+chimrom@users.noreply.github.com>
2023-02-09 17:34:21 +0300

    1 more

--- src/Components/NavBar/NavBarItem/NavBarItem.jsx
+++ src/Components/NavBar/NavBarItem/NavBarItem.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-import styles from "./NavBarItem.module.css"
+import styles from "./NavBarItem.module.css";
 
-const NavBarItem = ({text, path}) => <li className={styles.nav_bar_item}>
-    <a path={path}>{text}</a>
-</li>
+const NavBarItem = ({ text, path }) => (
+  <li className={styles.nav_bar_item}>
+    <a href={path}>{text}</a>
+  </li>
+);
 
-export default NavBarItem
+export default NavBarItem;
