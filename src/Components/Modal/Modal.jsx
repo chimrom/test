d879cace5d4b3568a4b91feffb82ea7497033d5e
chimrom <116731372+chimrom@users.noreply.github.com>
2023-02-10 11:44:53 +0300

    change flex-end

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,21 +1,21 @@
-import React from "react";
+// import React from "react";
 
-import "./Modal.css";
+// import "./Modal.css";
 
-const Modal = ({ isActive, setIsActive, children }) => {
-  return (
-    <div
-      className={isActive ? "modal active" : "modal"}
-      onClick={() => setIsActive(false)}
-    >
-      <div
-        className={isActive ? "modal_content active" : "modal_content"}
-        onClick={(e) => e.stopPropagation()}
-      >
-        {children}
-      </div>
-    </div>
-  );
-};
+// const Modal = ({ isActive, setIsActive, children }) => {
+//   return (
+//     <div
+//       className={isActive ? "modal active" : "modal"}
+//       onClick={() => setIsActive(false)}
+//     >
+//       <div
+//         className={isActive ? "modal_content active" : "modal_content"}
+//         onClick={(e) => e.stopPropagation()}
+//       >
+//         {children}
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Modal;
+// export default Modal;
